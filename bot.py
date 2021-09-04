a05f9fa5080121f3552faced8881920dce25c51a
maniatro111 <florin.postolache80@gmail.com>
2021-09-04 14:04:58 +0300

    solved

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 
 from music_cog import music_cog
 
-Bot = commands.Bot(command_prefix='.')
+Bot = commands.Bot(command_prefix='/')
 
 Bot.add_cog(music_cog(Bot))
 
@@ -21,7 +21,7 @@ async def on_message(message):
     if message.content.startswith('$salut'):
        await message.channel.send('hey, boss!')
 
-@Bot.event
+'''@Bot.event
 async def on_message(message):
     if message.content.startswith('$greet'):
         channel = message.channel
@@ -31,10 +31,10 @@ async def on_message(message):
             return m.content == 'hello' and m.channel == channel
 
         msg = await Bot.wait_for('message', check=check)
-        await channel.send('Hello {.author} on channel {.channel}!'.format(msg, msg))
+        await channel.send('Hello {.author} on channel {.channel}!'.format(msg, msg))'''
 
 token = ""
-with open("token1") as file:
+with open("token") as file:
     token = file.read()
 
 
